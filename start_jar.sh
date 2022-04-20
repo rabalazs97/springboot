2cd7b4303d341836438225195129d63481d6bb39
rabalazs97 <rabalazs97@gmail.com>
2022-04-20 09:31:39 +0200

    jar_start_script

--- start_jar.sh
+++ start_jar.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-nohup java -jar /home/cloud/onlab/onlab-0.0.1-SNAPSHOT.jar &
+nohup java -jar /home/cloud/onlab/onlab-0.0.1-SNAPSHOT.jar > /dev/null 2>&1 &
 echo $! >> /home/cloud/onlab/onlab.pid
-sleep 5
--- stop_jar.sh
+++ stop_jar.sh
@@ -2,7 +2,7 @@
 if apid=$(pgrep -f onlab-0.0.1-SNAPSHOT.jar)
 then
     echo "Running, pid is $apid"
-    sudo kill $apid
+    kill $apid
 else
     echo "Stopped"
 fi
